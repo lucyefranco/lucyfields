e76cec040519fca241f5547866991fab1a63f690
lucyefranco <lucyefranco@gmail.com>
2023-10-12 14:10:55 -0600

    removing header from home page

--- lucyfields/src/app/page.tsx
+++ lucyfields/src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from './components/header'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Header />
+      <p>Welcome home</p>
     </main>
   )
 }
