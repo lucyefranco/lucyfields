24e868aaa04f9297e80f19d5a8ff348e2c746033
lucyefranco <lucyefranco@gmail.com>
2023-10-12 14:11:27 -0600

    moving about page to correct location

---
+++ lucyfields/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import styles from '../'
+import style from '../page.module.css'
+
+export default function About() {
+    return (
+        <main className={style.main}>
+      <p>Welcome to about</p>
+    </main>
+    )
+  }
