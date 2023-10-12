9e0066b0dfb7ba880c4e3d60b475734858bfa43f
lucyefranco <lucyefranco@gmail.com>
2023-10-12 13:33:14 -0600

    import navbar into header

--- lucyfields/src/app/components/header.tsx
+++ lucyfields/src/app/components/header.tsx
@@ -1,11 +1,14 @@
+import NavBar from './navbar'
+
 export default function Header() {
   return (
       <div>
         <h1
         // clickable routing to home
         >Lucy Fields</h1>
-        <p>Coming Soon...</p>
-        {/* navigation bar */}
+        <div>
+          <NavBar />
+        </div>
       </div>
   )
 }
