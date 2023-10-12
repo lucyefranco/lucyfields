70cfd6748e94a6c34ec3fc0c14947f4335ba412c
lucyefranco <lucyefranco@gmail.com>
2023-10-12 14:13:16 -0600

    adding links to navbar

--- lucyfields/src/app/components/navbar.tsx
+++ lucyfields/src/app/components/navbar.tsx
@@ -1,6 +1,13 @@
+import Link from 'next/link'
 export default function NavBar() {
     return (
-        <div>
+        <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+            <nav style={{display:'flex', justifyContent:'space-between'}}>
+                <Link href='/'> Home </Link>
+                <Link href='/about'> About </Link>
+                <Link href='/blog'> Blog </Link>
+                <Link href='/contact'> Contact </Link>
+            </nav>
         </div>
     )
   }
