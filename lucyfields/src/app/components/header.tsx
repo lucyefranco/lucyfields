54c6a88fd79154817a7fcb245393d0b61400e3ee
lucyefranco <lucyefranco@gmail.com>
2023-10-12 12:57:56 -0600

    notes for future development

--- lucyfields/src/app/components/header.tsx
+++ lucyfields/src/app/components/header.tsx
@@ -1,8 +1,11 @@
 export default function Header() {
   return (
       <div>
-        <h1>Lucy Fields</h1>
+        <h1
+        // clickable routing to home
+        >Lucy Fields</h1>
         <p>Coming Soon...</p>
+        {/* navigation bar */}
       </div>
   )
 }
