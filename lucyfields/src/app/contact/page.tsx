d73a66d1ee43a3d6dde645d03ae0c1a288f91aa5
lucyefranco <lucyefranco@gmail.com>
2023-10-13 11:44:54 -0600

    adding contact page

---
+++ lucyfields/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import style from '../page.module.css'
+
+export default function Contact() {
+    return (
+        <main className={style.main}>
+      <p>Welcome to contact</p>
+    </main>
+    )
+  }
