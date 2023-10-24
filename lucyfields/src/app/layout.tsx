f5678948f9f735220c0965c028d6a496d7807330
lucyefranco <lucyefranco@gmail.com>
2023-10-24 12:06:19 -0600

    wrap application in context

--- lucyfields/src/app/layout.tsx
+++ lucyfields/src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from './components/header'
+import Context from "../../context/context"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,10 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      
-      <body className={inter.className}>
-      <Header />
-        {children}</body>
+      <Context>
+        <body className={inter.className}>
+          <Header />
+          {children}
+        </body>
+      </Context>
     </html>
   )
 }
