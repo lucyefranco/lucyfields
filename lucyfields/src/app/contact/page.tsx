d55d80490671ca3426f5d37b4b70b26893546de1
lucyefranco <lucyefranco@gmail.com>
2023-11-15 12:19:25 -0700

    import react-icons

--- lucyfields/src/app/contact/page.tsx
+++ lucyfields/src/app/contact/page.tsx
@@ -1,9 +1,22 @@
 import style from '../page.module.css'
+import { FaGithub } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+import { IoDocumentOutline } from "react-icons/io5";
 
 export default function Contact() {
     return (
         <main className={style.main}>
-      <p>Welcome to contact</p>
+      
+      <h1>Let's Keep in Touch</h1>
+        <MdOutlineEmail />
+        <p>Email</p>
+        <FaLinkedin />
+        <p>Linkedin</p>
+        <FaGithub />
+        <p>Github</p>
+        <IoDocumentOutline />
+        <p>Resume</p>
     </main>
     )
   }
